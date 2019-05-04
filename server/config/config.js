cd370ca56549e8f0061c8a7cf38bb56422d187d4
Aman2011 <amanrawal84@gmail.com>
2019-05-04 20:06:35 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -7,26 +7,26 @@ module.exports = {
         website: 'http://localhost:7700',
         rootPath: rootPath,
         port: process.env.PORT || 7700,
-        email: 'amanrawal84@gmail.com',
-        smtp_pass: 'GameOver2011',
+        email: 'xxxxx@gmail.com',
+        smtp_pass: 'xxxxxx',
         linkedInAuth: {
-            'clientID': '77knpgir45dxaf',
-            'clientSecret': 'nDEKILjFBCIqdCDe',
+            'clientID': 'xxxxx',
+            'clientSecret': 'xxxxxx',
             'callbackURL': 'http://localhost:7700/auth/linkedIn/callback'
         },
         verifyUrlPrefix: "http://localhost:7700/"
 
     },
     production: {
-        db: 'mongodb://admin:Hack2011@ds163667.mlab.com:63667/hackconnect',
+        db: 'mongodb://username:password@ds163667.mlab.com:63667/hackconnect',
         website: 'https://hackconnect.herokuapp.com',
         rootPath: rootPath,
         port: process.env.PORT || 80,
-        email: 'amanrawal84@gmail.com',
-        smtp_pass: 'GameOver2011',
+        email: 'xxxxx',
+        smtp_pass: 'xxxxx',
         linkedInAuth: {
-            'clientID': '77knpgir45dxaf',
-            'clientSecret': 'nDEKILjFBCIqdCDe',
+            'clientID': 'xxxxx',
+            'clientSecret': 'xxxxxx',
             'callbackURL': 'https://hackconnect.herokuapp.com/auth/linkedIn/callback'
         },
         verifyUrlPrefix: "http://hackconnect.herokuapp.com/"
